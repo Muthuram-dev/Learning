a44551d073b390c0e3576b3fd81bbd81db74fddb
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-03-12 11:25:40 +0530

    bubblingbasics

--- DOM/DOM_Events_CODE/Bubbling/app.js
+++ DOM/DOM_Events_CODE/Bubbling/app.js
@@ -1,17 +1,17 @@
-const button = document.querySelector('#changeColor');
-const container = document.querySelector('#container');
+const randomColor = () =>{
+    const color1 = Math.floor(Math.random() * 255);
+    const color2 = Math.floor(Math.random() * 255);
+    const color3 = Math.floor(Math.random() * 255);
 
-button.addEventListener('click', function (e) {
-    container.style.backgroundColor = makeRandColor();
-    e.stopPropagation();
-})
-container.addEventListener('click', function () {
-    container.classList.toggle('hide');
-})
-
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${color1},${color2},${color3}`
 }
+
+const button = document.querySelector("#changeColor");
+const container = document.querySelector("#container");
+container.addEventListener("click", function(){
+    container.classList.toggle("hide");
+})
+
+button.addEventListener("click", ()=>{
+    container.style.backgroundColor = randomColor();
+})
--- DOM/DOM_Events_CODE/FormEvents/app.js
+++ DOM/DOM_Events_CODE/FormEvents/app.js
@@ -1,10 +1,15 @@
 const form = document.querySelector("#sform");
 const input = document.querySelector("input");
 const list = document.querySelector("#ul");
+
+
 form.addEventListener("submit", function(e){
+    
     e.preventDefault();
     const catName = input.value;
+
     const newul = document.createElement("LI");
+
     newul.innerText = catName;
     list.appendChild(newul);
 })
