c03a693d61594107b9ffbdaa3c91e188724ddd2d
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-03-12 06:24:27 +0530

    DOM events

--- DOM/DOM_Events_CODE/FormEvents/app.js
+++ DOM/DOM_Events_CODE/FormEvents/app.js
@@ -1,5 +1,10 @@
 const form = document.querySelector("#sform");
+const input = document.querySelector("input");
+const list = document.querySelector("#ul");
 form.addEventListener("submit", function(e){
-    console.log("SUBMITTED");
     e.preventDefault();
+    const catName = input.value;
+    const newul = document.createElement("LI");
+    newul.innerText = catName;
+    list.appendChild(newul);
 })
