d1dbb7d69778fd7525b156a3b15e35785f23b767
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-03-24 18:16:27 +0530

    Bubbling and forms

--- DOM/DOM_Events_CODE/Bubbling/app.js
+++ DOM/DOM_Events_CODE/Bubbling/app.js
@@ -3,15 +3,20 @@ const randomColor = () =>{
     const color2 = Math.floor(Math.random() * 255);
     const color3 = Math.floor(Math.random() * 255);
 
-    return `rgb(${color1},${color2},${color3}`
+    return `rgb(${color1}, ${color2}, ${color3}`;
 }
 
 const button = document.querySelector("#changeColor");
 const container = document.querySelector("#container");
+
+
+button.addEventListener("click", function(e){
+    container.style.backgroundColor = randomColor();
+    e.stopPropagation();
+})
+
+
 container.addEventListener("click", function(){
     container.classList.toggle("hide");
 })
 
-button.addEventListener("click", ()=>{
-    container.style.backgroundColor = randomColor();
-})
