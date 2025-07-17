4c433d284329be621163523a4f9e5dab2e57344f
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-07-17 11:06:17 +0530

    node intro and temp-converter project

--- Projects/temp-converter/app.js
+++ Projects/temp-converter/app.js
@@ -1,33 +1,42 @@
-const celsius = document.querySelector("#celcius");
-const Farenheit = document.querySelector("#Farenheit");
-const kelvin = document.querySelector("#Kelvin");
-const form = document.querySelector("form");
+const celsius = document.querySelector("#celsius");
+const fahrenheit = document.querySelector("#fahrenheit");
+const kelvin = document.querySelector("#kelvin");
+const clearBtn = document.querySelector("#clear");
 
-const celsiusNum = celsius.parseFloat(celsius.value)
-const farenheitNum = Farenheit.parseFloat(Farenheit.value)
-const kelvinNum = Farenheit.parseFloat(kelvin.value)
+celsius.addEventListener("input", function () {
+    const val = parseFloat(celsius.value);
+    if (!isNaN(val)) {
+        fahrenheit.value = ((val * 9/5) + 32).toFixed(2);
+        kelvin.value = (val + 273.15).toFixed(2);
+    } else {
+        alert("Please enter a valid number");
+    }
+});
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
+fahrenheit.addEventListener("input", function () {
+    const val = parseFloat(fahrenheit.value);
+    if (!isNaN(val)) {
+        celsius.value = ((val - 32) * 5/9).toFixed(2);
+        kelvin.value = (((val - 32) * 5/9) + 273.15).toFixed(2);
+    } else {
+        alert("Please enter a valid number");
+    }
+});
 
+kelvin.addEventListener("input", function () {
+    const val = parseFloat(kelvin.value);
+    if (!isNaN(val)) {
+        celsius.value = (val - 273.15).toFixed(2);
+        fahrenheit.value = ((val - 273.15) * 9/5 + 32).toFixed(2);
+    } else {
+        alert("Please enter a valid number");
+    }
+});
+
+
+clearBtn.addEventListener("click", function(){
+  fahrenheit.value = "";
+  kelvin.value = "";
+  celsius.value = "";
 })
 
-
-function celsiusToFahrenheit(celsiusNum) {
-  return (celsiusNum * 9/5) + 32;
-}
-function fahrenheitToCelsius(farenheitNum) {
-  return (farenheitNum - 32) * 5/9;
-}
-function celsiusToKelvin(celsiusNum) {
-  return celsiusNum + 273.15;
-}
-function kelvinToCelsius(kelvinNum) {
-  return kelvinNum - 273.15;
-}
-function fahrenheitToKelvin(fahrenheitNum) {
-  return (fahrenheitNum - 32) * 5/9 + 273.15;
-}
-function kelvinToFahrenheit(kelvinNum) {
-  return (kelvinNum - 273.15) * 9/5 + 32;
-}
