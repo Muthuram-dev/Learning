c16a3b08f9586a2f336c2e963ddf4319f750591d
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-07-29 21:14:33 +0530

    Express: req method in usefunction

--- Express/firstProject/index.js
+++ Express/firstProject/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res) => {
+    // console.dir(req);
+    // console.log("You got a new request!")
+    res.send("hello, we got the request")
+})
+
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
-
-app.use((req, res) => {
-    console.dir(req);
-})
