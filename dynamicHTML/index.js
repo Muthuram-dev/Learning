69c87349cf4a408de7b712135599a2d993a97273
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-09-13 13:41:15 +0530

    EJS conditionals and loops in templating

--- dynamicHTML/index.js
+++ dynamicHTML/index.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const path = require("path");
+const data = require("./data.json")
 const app = express();
 
 
 
 
+
+
+
 app.set("view engine", "ejs");
+
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+})
+
+app.get("/cats", (req, res) => {
+    // res.send("Welcome to the Cats page!");
+    const cats = ["hailey", "daisy", "chigga", "nuggets", "pumpkins"];
+    res.render("cats", {kitties : cats});
+})
+
+app.get("/r/:subreddit", (req, res) =>{
+    const { subreddit } = req.params;
+    const subredditData = data[subreddit];
+    console.log(subredditData);
+    res.render("subreddit", {subreddit});
+    
 })
 
 app.get("/random", (req, res) => {
