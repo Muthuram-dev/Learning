a799d77821e551a24c25a7e0245cb4d596b8141a
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-09-16 19:59:50 +0530

    EJS partials, Serving static page using the public folder, some complex templating

--- dynamicHTML/index.js
+++ dynamicHTML/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const data = require("./data.json")
 const app = express();
 
+app.use(express.static(path.join(__dirname, "/public")))
+
 
 
 
@@ -13,31 +15,38 @@ app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "/views"));
 
+
 app.get("/", (req, res) => {
-    res.render("home");
+    const {logoName} = req.params;
+    res.render("home", {logoName});
 })
+
+
 
 app.get("/cats", (req, res) => {
     // res.send("Welcome to the Cats page!");
+    const {logoName} = req.params;
     const cats = ["hailey", "daisy", "chigga", "nuggets", "pumpkins"];
-    res.render("cats", {kitties : cats});
+    res.render("cats", {kitties : cats, logoName});
 })
 
-app.get("/r/:subreddit", (req, res) =>{
-    const { subreddit } = req.params;
-    const subredditData = data[subreddit];
+app.get("/r/:logoName", (req, res) =>{
+    const { logoName } = req.params;
+    const { ...subredditData} = data[logoName];
     console.log(subredditData);
-    res.render("subreddit", {subreddit});
+    res.render("subreddit", {logoName, subredditData:subredditData});
     
 })
 
 app.get("/random", (req, res) => {
+    const {logoName} = req.params;
     const randNum = Math.floor(Math.random() * 10) +1;
-    res.render("random", {random: randNum});
+    res.render("random", {logoName, random: randNum});
 })
 
 app.get(/(.*)/, (req, res) => {
-    res.render("errorPage");
+    const {logoName} = req.params;
+    res.render("errorPage", {logoName});
 })
 
 app.listen("3000", () => {
